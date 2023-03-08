86b87a0220775501f86d1aaa90927ee007b0ad5b
kaiserdem <>
2023-03-08 10:06:31 +0200

    init project with assets and setting

---
+++ MyCrypto/MyCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCryptoApp.swift
+//  MyCrypto
+//
+//  Created by kaiserdem  on 08.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
