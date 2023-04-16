d15e0147f22deb56d9e65431ed9cf5258b01ff21
kaiserdem <>
2023-04-16 12:26:54 +0300

    Saving images to the File Manager

--- MyCrypto/Services/CoinImageService.swift
+++ MyCrypto/Services/CoinImageService.swift
@@ -14,13 +14,27 @@ class CoinImageService {
     @Published var image: UIImage? = nil
     private var imageSubscriptions: AnyCancellable?
     private let coin: CoinModel
-    
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
+
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage  = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            ///print("Retrived image from File Manager!")
+        } else {
+            downlodCoinImage()
+            ///print("Download image now!")
+        }
+    }
+    
+    private func downlodCoinImage() {
         guard let url = URL(string: coin.image) else { return }
      
         imageSubscriptions = NetworkingManager.download(url: url)
@@ -28,8 +42,10 @@ class CoinImageService {
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage in
-                self?.image = returnedImage
-                self?.imageSubscriptions?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = returnedImage
+                self.imageSubscriptions?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
---
+++ MyCrypto/Utils/LocalFileManager.swift
@@ -0,0 +1,68 @@
+//
+//  LocalFileManager.swift
+//  MyCrypto
+//
+//  Created by kaiserdem  on 13.04.2023.
+//
+
+import UIKit
+
+class LocalFileManager {
+    
+    static var instance = LocalFileManager()
+    private init() {}
+    
+    func saveImage(image: UIImage, imageName: String, folderName: String) {
+        
+        // create folder
+        createFolderIfNeeded(folderName: folderName)
+        
+        
+        // get path for image
+        guard
+            let data = image.pngData(),
+            let url = getUrlForImage(imageName: imageName, folderName: folderName)
+        else { return }
+        
+        // save image to path
+        do  {
+            try data.write(to: url)
+        } catch let error {
+            print("Error saving image. Image name: \(imageName). \(error)")
+        }
+    }
+    
+    func getImage(imageName: String, folderName: String) -> UIImage? {
+        guard
+            let url = getUrlForImage(imageName: imageName, folderName: folderName),
+            FileManager.default.fileExists(atPath: url.path) else {
+        return nil
+        }
+        return UIImage(contentsOfFile: url.path)
+    }
+    
+    private func createFolderIfNeeded(folderName: String) {
+        guard let url = getUrlForFolder(folderName: folderName) else { return }
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("Error creating directory. Folder name: \(folderName). \(error)")
+            }
+        }
+    }
+    
+    private func getUrlForFolder(folderName: String) -> URL? {
+        guard let url = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        return url.appendingPathExtension(folderName)
+    }
+    
+    private func getUrlForImage(imageName: String, folderName: String) -> URL? {
+        guard let folderUrl = getUrlForFolder(folderName: folderName) else {
+            return nil
+        }
+        return folderUrl.appendingPathExtension(imageName + ".png")
+    }
+}
