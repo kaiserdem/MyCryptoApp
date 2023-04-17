5b89ee38ff4ed48ebdafb631b80294d6ff25e8a7
kaiserdem <>
2023-04-17 11:13:40 +0300

    custom and reusable search bar

--- MyCrypto/Core/Home/Views/HomeView.swift
+++ MyCrypto/Core/Home/Views/HomeView.swift
@@ -21,7 +21,7 @@ struct HomeView: View {
             // content leyer
             VStack {
                 homeHeader
-                
+                SearchBarView(searchText: $vm.searchText)
                 columnTitles
                 
                 if !shadowPortfolio {
