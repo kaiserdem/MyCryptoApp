81ba15bc6c50596ad3319ce9b8b6f9b23b3cd716
kaiserdem <>
2023-04-20 10:45:06 +0300

    filtering data based on search bar text

--- MyCrypto/Core/Home/ViewModels/HomeViewModel.swift
+++ MyCrypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -22,10 +22,36 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
+        
+        // updates all coins
         dataService.$allCoins
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        //
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main) // waiting 0.5 sec for launch next code
+            .map(filterCoins)
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowecasedText = text.lowercased()
+        
+        return coins.filter { coin in
+            return coin.name.lowercased().contains(lowecasedText) ||
+            coin.symbol.lowercased().contains(lowecasedText) ||
+            coin.id.lowercased().contains(lowecasedText)
+        }
     }
 }
